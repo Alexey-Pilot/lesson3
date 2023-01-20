5791a195ebfe8ed7a073a6ae2a11484021c9e36f
Alexey <alexragnarek@gmail.com>
2023-01-20 15:07:13 +0500

    Ex21 is complited

---
+++ ex21/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите координаты Х первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты Y первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты Z первой точки: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты Х второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты Y второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты Z второй точки: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double len = Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1-z2, 2)),2);
+Console.WriteLine($"Расстояние между точками равняется {len}");
